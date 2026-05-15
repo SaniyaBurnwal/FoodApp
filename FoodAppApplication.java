b81f55fa26b10d776f7ab43b44ea7e75c5d5ad0d
Saniya Bharti <saniyaburnwal@gmail.com>
2026-05-15 22:34:21 +0530

    Add files via upload

---
+++ FoodAppApplication.java
@@ -0,0 +1,22 @@
+package com.phegon.FoodApp;
+
+import com.phegon.FoodApp.email_notification.dtos.NotificationDTO;
+import com.phegon.FoodApp.email_notification.services.NotificationService;
+import com.phegon.FoodApp.enums.NotificationType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class FoodAppApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppApplication.class, args);
+	}
+
+}
