0a931d4ca8969251263e56a8afdc7bc211b0b63a
Saniya Bharti <saniyaburnwal@gmail.com>
2026-07-19 13:39:41 +0530

    Add files via upload

--- Document-a8d0fbf9.js
+++ Document-a8d0fbf9.js
@@ -1,8 +1,8 @@
-import { parse as parse$1 } from './parse-cst.js';
-import { d as defaultTagPrefix, _ as _defineProperty, T as Type, Y as YAMLSyntaxError, a as YAMLWarning, b as YAMLSemanticError, c as YAMLError } from './PlainValue-183afbad.js';
-import { b as binaryOptions, a as boolOptions, i as intOptions, n as nullOptions, s as strOptions, N as Node, P as Pair, S as Scalar, c as stringifyString, A as Alias, Y as YAMLSeq, d as YAMLMap, M as Merge, C as Collection, r as resolveNode, e as isEmptyPath, t as toJSON, f as addComment } from './resolveSeq-67caf78a.js';
-import { S as Schema } from './Schema-9530c078.js';
-import { w as warn } from './warnings-5e4358fe.js';
+'use strict';
+
+var PlainValue = require('./PlainValue-516d5bc2.js');
+var resolveSeq = require('./resolveSeq-95613e94.js');
+var Schema = require('./Schema-bcc6c2d7.js');
 
 const defaultOptions = {
   anchorPrefix: 'a',
@@ -21,34 +21,34 @@ const defaultOptions = {
 };
 const scalarOptions = {
   get binary() {
-    return binaryOptions;
+    return resolveSeq.binaryOptions;
   },
   set binary(opt) {
-    Object.assign(binaryOptions, opt);
+    Object.assign(resolveSeq.binaryOptions, opt);
   },
   get bool() {
-    return boolOptions;
+    return resolveSeq.boolOptions;
   },
   set bool(opt) {
-    Object.assign(boolOptions, opt);
+    Object.assign(resolveSeq.boolOptions, opt);
   },
   get int() {
-    return intOptions;
+    return resolveSeq.intOptions;
   },
   set int(opt) {
-    Object.assign(intOptions, opt);
+    Object.assign(resolveSeq.intOptions, opt);
   },
   get null() {
-    return nullOptions;
+    return resolveSeq.nullOptions;
   },
   set null(opt) {
-    Object.assign(nullOptions, opt);
+    Object.assign(resolveSeq.nullOptions, opt);
   },
   get str() {
-    return strOptions;
+    return resolveSeq.strOptions;
   },
   set str(opt) {
-    Object.assign(strOptions, opt);
+    Object.assign(resolveSeq.strOptions, opt);
   }
 };
 const documentOptions = {
@@ -57,7 +57,7 @@ const documentOptions = {
     merge: true,
     tagPrefixes: [{
       handle: '!',
-      prefix: defaultTagPrefix
+      prefix: PlainValue.defaultTagPrefix
     }, {
       handle: '!!',
       prefix: 'tag:private.yaml.org,2002:'
@@ -71,7 +71,7 @@ const documentOptions = {
       prefix: '!'
     }, {
       handle: '!!',
-      prefix: defaultTagPrefix
+      prefix: PlainValue.defaultTagPrefix
     }]
   },
   1.2: {
@@ -82,7 +82,7 @@ const documentOptions = {
       prefix: '!'
     }, {
       handle: '!!',
-      prefix: defaultTagPrefix
+      prefix: PlainValue.defaultTagPrefix
     }]
   }
 };
@@ -112,13 +112,13 @@ function stringifyTag(doc, tag) {
 }
 
 function getTagObject(tags, item) {
-  if (item instanceof Alias) return Alias;
+  if (item instanceof resolveSeq.Alias) return resolveSeq.Alias;
   if (item.tag) {
     const match = tags.filter(t => t.tag === item.tag);
     if (match.length > 0) return match.find(t => t.format === item.format) || match[0];
   }
   let tagObj, obj;
-  if (item instanceof Scalar) {
+  if (item instanceof resolveSeq.Scalar) {
     obj = item.value;
     // TODO: deprecate/remove class check
     const match = tags.filter(t => t.identify && t.identify(obj) || t.class && obj instanceof t.class);
@@ -152,13 +152,13 @@ function stringifyProps(node, tagObj, {
   }
   return props.join(' ');
 }
-function stringify$1(item, ctx, onComment, onChompKeep) {
+function stringify(item, ctx, onComment, onChompKeep) {
   const {
     anchors,
     schema
   } = ctx.doc;
   let tagObj;
-  if (!(item instanceof Node)) {
+  if (!(item instanceof resolveSeq.Node)) {
     const createCtx = {
       aliasNodes: [],
       onTagObj: o => tagObj = o,
@@ -174,33 +174,33 @@ function stringify$1(item, ctx, onComment, onChompKeep) {
       }
     }
   }
-  if (item instanceof Pair) return item.toString(ctx, onComment, onChompKeep);
+  if (item instanceof resolveSeq.Pair) return item.toString(ctx, onComment, onChompKeep);
   if (!tagObj) tagObj = getTagObject(schema.tags, item);
   const props = stringifyProps(item, tagObj, ctx);
   if (props.length > 0) ctx.indentAtStart = (ctx.indentAtStart || 0) + props.length + 1;
-  const str = typeof tagObj.stringify === 'function' ? tagObj.stringify(item, ctx, onComment, onChompKeep) : item instanceof Scalar ? stringifyString(item, ctx, onComment, onChompKeep) : item.toString(ctx, onComment, onChompKeep);
+  const str = typeof tagObj.stringify === 'function' ? tagObj.stringify(item, ctx, onComment, onChompKeep) : item instanceof resolveSeq.Scalar ? resolveSeq.stringifyString(item, ctx, onComment, onChompKeep) : item.toString(ctx, onComment, onChompKeep);
   if (!props) return str;
-  return item instanceof Scalar || str[0] === '{' || str[0] === '[' ? `${props} ${str}` : `${props}\n${ctx.indent}${str}`;
+  return item instanceof resolveSeq.Scalar || str[0] === '{' || str[0] === '[' ? `${props} ${str}` : `${props}\n${ctx.indent}${str}`;
 }
 
 class Anchors {
   static validAnchorNode(node) {
-    return node instanceof Scalar || node instanceof YAMLSeq || node instanceof YAMLMap;
+    return node instanceof resolveSeq.Scalar || node instanceof resolveSeq.YAMLSeq || node instanceof resolveSeq.YAMLMap;
   }
   constructor(prefix) {
-    _defineProperty(this, "map", Object.create(null));
+    PlainValue._defineProperty(this, "map", Object.create(null));
     this.prefix = prefix;
   }
   createAlias(node, name) {
     this.setAnchor(node, name);
-    return new Alias(node);
+    return new resolveSeq.Alias(node);
   }
   createMergePair(...sources) {
-    const merge = new Merge();
+    const merge = new resolveSeq.Merge();
     merge.value.items = sources.map(s => {
-      if (s instanceof Alias) {
-        if (s.source instanceof YAMLMap) return s;
-      } else if (s instanceof YAMLMap) {
+      if (s instanceof resolveSeq.Alias) {
+        if (s.source instanceof resolveSeq.YAMLMap) return s;
+      } else if (s instanceof resolveSeq.YAMLMap) {
         return this.createAlias(s);
       }
       throw new Error('Merge sources must be Map nodes or their Aliases');
@@ -276,13 +276,13 @@ const visit = (node, tags) => {
     const {
       tag
     } = node;
-    if (node instanceof Collection) {
+    if (node instanceof resolveSeq.Collection) {
       if (tag) tags[tag] = true;
       node.items.forEach(n => visit(n, tags));
-    } else if (node instanceof Pair) {
+    } else if (node instanceof resolveSeq.Pair) {
       visit(node.key, tags);
       visit(node.value, tags);
-    } else if (node instanceof Scalar) {
+    } else if (node instanceof resolveSeq.Scalar) {
       if (tag) tags[tag] = true;
     }
   }
@@ -301,10 +301,10 @@ function parseContents(doc, contents) {
     if (node.valueRange) {
       if (body !== undefined) {
         const msg = 'Document contains trailing content not separated by a ... or --- line';
-        doc.errors.push(new YAMLSyntaxError(node, msg));
+        doc.errors.push(new PlainValue.YAMLSyntaxError(node, msg));
         break;
       }
-      const res = resolveNode(doc, node);
+      const res = resolveSeq.resolveNode(doc, node);
       if (spaceBefore) {
         res.spaceBefore = true;
         spaceBefore = false;
@@ -313,7 +313,7 @@ function parseContents(doc, contents) {
     } else if (node.comment !== null) {
       const cc = body === undefined ? comments.before : comments.after;
       cc.push(node.comment);
-    } else if (node.type === Type.BLANK_LINE) {
+    } else if (node.type === PlainValue.Type.BLANK_LINE) {
       spaceBefore = true;
       if (body === undefined && comments.before.length > 0 && !doc.commentBefore) {
         // space-separated comments at start are parsed as document comments
@@ -328,7 +328,7 @@ function parseContents(doc, contents) {
   } else {
     const cb = comments.before.join('\n');
     if (cb) {
-      const cbNode = body instanceof Collection && body.items[0] ? body.items[0] : body;
+      const cbNode = body instanceof resolveSeq.Collection && body.items[0] ? body.items[0] : body;
       cbNode.commentBefore = cbNode.commentBefore ? `${cb}\n${cbNode.commentBefore}` : cb;
     }
     doc.comment = comments.after.join('\n') || null;
@@ -341,11 +341,11 @@ function resolveTagDirective({
   const [handle, prefix] = directive.parameters;
   if (!handle || !prefix) {
     const msg = 'Insufficient parameters given for %TAG directive';
-    throw new YAMLSemanticError(directive, msg);
+    throw new PlainValue.YAMLSemanticError(directive, msg);
   }
   if (tagPrefixes.some(p => p.handle === handle)) {
     const msg = 'The %TAG directive must only be given at most once per handle in the same document.';
-    throw new YAMLSemanticError(directive, msg);
+    throw new PlainValue.YAMLSemanticError(directive, msg);
   }
   return {
     handle,
@@ -357,12 +357,12 @@ function resolveYamlDirective(doc, directive) {
   if (directive.name === 'YAML:1.0') version = '1.0';
   if (!version) {
     const msg = 'Insufficient parameters given for %YAML directive';
-    throw new YAMLSemanticError(directive, msg);
+    throw new PlainValue.YAMLSemanticError(directive, msg);
   }
   if (!documentOptions[version]) {
     const v0 = doc.version || doc.options.version;
     const msg = `Document will be parsed as YAML ${v0} rather than YAML ${version}`;
-    doc.warnings.push(new YAMLWarning(directive, msg));
+    doc.warnings.push(new PlainValue.YAMLWarning(directive, msg));
   }
   return version;
 }
@@ -387,7 +387,7 @@ function parseDirectives(doc, directives, prevDoc) {
       case 'YAML:1.0':
         if (doc.version) {
           const msg = 'The %YAML directive must only be given at most once per document.';
-          doc.errors.push(new YAMLSemanticError(directive, msg));
+          doc.errors.push(new PlainValue.YAMLSemanticError(directive, msg));
         }
         try {
           doc.version = resolveYamlDirective(doc, directive);
@@ -399,7 +399,7 @@ function parseDirectives(doc, directives, prevDoc) {
       default:
         if (name) {
           const msg = `YAML only supports %TAG and %YAML directives, and not %${name}`;
-          doc.warnings.push(new YAMLWarning(directive, msg));
+          doc.warnings.push(new PlainValue.YAMLWarning(directive, msg));
         }
     }
     if (comment) directiveComments.push(comment);
@@ -419,10 +419,10 @@ function parseDirectives(doc, directives, prevDoc) {
 }
 
 function assertCollection(contents) {
-  if (contents instanceof Collection) return true;
+  if (contents instanceof resolveSeq.Collection) return true;
   throw new Error('Expected a YAML collection as document contents');
 }
-class Document$1 {
+class Document {
   constructor(options) {
     this.anchors = new Anchors(options.anchorPrefix);
     this.commentBefore = null;
@@ -449,7 +449,7 @@ class Document$1 {
     return this.contents.delete(key);
   }
   deleteIn(path) {
-    if (isEmptyPath(path)) {
+    if (resolveSeq.isEmptyPath(path)) {
       if (this.contents == null) return false;
       this.contents = null;
       return true;
@@ -458,28 +458,28 @@ class Document$1 {
     return this.contents.deleteIn(path);
   }
   getDefaults() {
-    return Document$1.defaults[this.version] || Document$1.defaults[this.options.version] || {};
+    return Document.defaults[this.version] || Document.defaults[this.options.version] || {};
   }
   get(key, keepScalar) {
-    return this.contents instanceof Collection ? this.contents.get(key, keepScalar) : undefined;
+    return this.contents instanceof resolveSeq.Collection ? this.contents.get(key, keepScalar) : undefined;
   }
   getIn(path, keepScalar) {
-    if (isEmptyPath(path)) return !keepScalar && this.contents instanceof Scalar ? this.contents.value : this.contents;
-    return this.contents instanceof Collection ? this.contents.getIn(path, keepScalar) : undefined;
+    if (resolveSeq.isEmptyPath(path)) return !keepScalar && this.contents instanceof resolveSeq.Scalar ? this.contents.value : this.contents;
+    return this.contents instanceof resolveSeq.Collection ? this.contents.getIn(path, keepScalar) : undefined;
   }
   has(key) {
-    return this.contents instanceof Collection ? this.contents.has(key) : false;
+    return this.contents instanceof resolveSeq.Collection ? this.contents.has(key) : false;
   }
   hasIn(path) {
-    if (isEmptyPath(path)) return this.contents !== undefined;
-    return this.contents instanceof Collection ? this.contents.hasIn(path) : false;
+    if (resolveSeq.isEmptyPath(path)) return this.contents !== undefined;
+    return this.contents instanceof resolveSeq.Collection ? this.contents.hasIn(path) : false;
   }
   set(key, value) {
     assertCollection(this.contents);
     this.contents.set(key, value);
   }
   setIn(path, value) {
-    if (isEmptyPath(path)) this.contents = value;else {
+    if (resolveSeq.isEmptyPath(path)) this.contents = value;else {
       assertCollection(this.contents);
       this.contents.setIn(path, value);
     }
@@ -495,7 +495,7 @@ class Document$1 {
     }
     if (Array.isArray(customTags)) this.options.customTags = customTags;
     const opt = Object.assign({}, this.getDefaults(), this.options);
-    this.schema = new Schema(opt);
+    this.schema = new Schema.Schema(opt);
   }
   parse(node, prevDoc) {
     if (this.options.keepCstNodes) this.cstNode = node;
@@ -519,13 +519,13 @@ class Document$1 {
     parseContents(this, contents);
     this.anchors.resolveNodes();
     if (this.options.prettyErrors) {
-      for (const error of this.errors) if (error instanceof YAMLError) error.makePretty();
-      for (const warn of this.warnings) if (warn instanceof YAMLError) warn.makePretty();
+      for (const error of this.errors) if (error instanceof PlainValue.YAMLError) error.makePretty();
+      for (const warn of this.warnings) if (warn instanceof PlainValue.YAMLError) warn.makePretty();
     }
     return this;
   }
   listNonDefaultTags() {
-    return listTagNames(this.contents).filter(t => t.indexOf(Schema.defaultPrefix) !== 0);
+    return listTagNames(this.contents).filter(t => t.indexOf(Schema.Schema.defaultPrefix) !== 0);
   }
   setTagPrefix(handle, prefix) {
     if (handle[0] !== '!' || handle[handle.length - 1] !== '!') throw new Error('Handle must start and end with !');
@@ -545,14 +545,14 @@ class Document$1 {
       mapAsMap,
       maxAliasCount
     } = this.options;
-    const keep = keepBlobsInJSON && (typeof arg !== 'string' || !(this.contents instanceof Scalar));
+    const keep = keepBlobsInJSON && (typeof arg !== 'string' || !(this.contents instanceof resolveSeq.Scalar));
     const ctx = {
       doc: this,
       indentStep: '  ',
       keep,
       mapAsMap: keep && !!mapAsMap,
       maxAliasCount,
-      stringify: stringify$1 // Requiring directly in Pair would create circular dependencies
+      stringify // Requiring directly in Pair would create circular dependencies
     };
     const anchorNames = Object.keys(this.anchors.map);
     if (anchorNames.length > 0) ctx.anchors = new Map(anchorNames.map(name => [this.anchors.map[name], {
@@ -560,7 +560,7 @@ class Document$1 {
       aliasCount: 0,
       count: 1
     }]));
-    const res = toJSON(this.contents, arg, ctx);
+    const res = resolveSeq.toJSON(this.contents, arg, ctx);
     if (typeof onAnchor === 'function' && ctx.anchors) for (const {
       count,
       res
@@ -605,12 +605,12 @@ class Document$1 {
       doc: this,
       indent: '',
       indentStep: ' '.repeat(indentSize),
-      stringify: stringify$1 // Requiring directly in nodes would create circular dependencies
+      stringify // Requiring directly in nodes would create circular dependencies
     };
     let chompKeep = false;
     let contentComment = null;
     if (this.contents) {
-      if (this.contents instanceof Node) {
+      if (this.contents instanceof resolveSeq.Node) {
         if (this.contents.spaceBefore && (hasDirectives || this.directivesEndMarker)) lines.push('');
         if (this.contents.commentBefore) lines.push(this.contents.commentBefore.replace(/^/gm, '#'));
         // top-level block scalars need to be indented if followed by a comment
@@ -618,10 +618,10 @@ class Document$1 {
         contentComment = this.contents.comment;
       }
       const onChompKeep = contentComment ? null : () => chompKeep = true;
-      const body = stringify$1(this.contents, ctx, () => contentComment = null, onChompKeep);
-      lines.push(addComment(body, '', contentComment));
+      const body = stringify(this.contents, ctx, () => contentComment = null, onChompKeep);
+      lines.push(resolveSeq.addComment(body, '', contentComment));
     } else if (this.contents !== undefined) {
-      lines.push(stringify$1(this.contents, ctx));
+      lines.push(stringify(this.contents, ctx));
     }
     if (this.comment) {
       if ((!chompKeep || contentComment) && lines[lines.length - 1] !== '') lines.push('');
@@ -630,63 +630,8 @@ class Document$1 {
     return lines.join('\n') + '\n';
   }
 }
-_defineProperty(Document$1, "defaults", documentOptions);
+PlainValue._defineProperty(Document, "defaults", documentOptions);
 
-function createNode(value, wrapScalars = true, tag) {
-  if (tag === undefined && typeof wrapScalars === 'string') {
-    tag = wrapScalars;
-    wrapScalars = true;
-  }
-  const options = Object.assign({}, Document$1.defaults[defaultOptions.version], defaultOptions);
-  const schema = new Schema(options);
-  return schema.createNode(value, wrapScalars, tag);
-}
-class Document extends Document$1 {
-  constructor(options) {
-    super(Object.assign({}, defaultOptions, options));
-  }
-}
-function parseAllDocuments(src, options) {
-  const stream = [];
-  let prev;
-  for (const cstDoc of parse$1(src)) {
-    const doc = new Document(options);
-    doc.parse(cstDoc, prev);
-    stream.push(doc);
-    prev = doc;
-  }
-  return stream;
-}
-function parseDocument(src, options) {
-  const cst = parse$1(src);
-  const doc = new Document(options).parse(cst[0]);
-  if (cst.length > 1) {
-    const errMsg = 'Source contains multiple documents; please use YAML.parseAllDocuments()';
-    doc.errors.unshift(new YAMLSemanticError(cst[1], errMsg));
-  }
-  return doc;
-}
-function parse(src, options) {
-  const doc = parseDocument(src, options);
-  doc.warnings.forEach(warning => warn(warning));
-  if (doc.errors.length > 0) throw doc.errors[0];
-  return doc.toJSON();
-}
-function stringify(value, options) {
-  const doc = new Document(options);
-  doc.contents = value;
-  return String(doc);
-}
-const YAML = {
-  createNode,
-  defaultOptions,
-  Document,
-  parse,
-  parseAllDocuments,
-  parseCST: parse$1,
-  parseDocument,
-  scalarOptions,
-  stringify
-};
-
-export { YAML };
+exports.Document = Document;
+exports.defaultOptions = defaultOptions;
+exports.scalarOptions = scalarOptions;
