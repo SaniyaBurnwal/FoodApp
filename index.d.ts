432a758e30d08aec749235d749469926f81fffd3
Saniya Bharti <saniyaburnwal@gmail.com>
2026-07-24 13:07:25 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,25 @@
-import { copyWorkboxLibraries } from './lib/copy-workbox-libraries';
-import { getModuleURL } from './lib/cdn-utils';
-import { generateSW } from './generate-sw';
-import { getManifest } from './get-manifest';
-import { injectManifest } from './inject-manifest';
+import { BackgroundSyncPlugin } from './BackgroundSyncPlugin.js';
+import { Queue, QueueOptions } from './Queue.js';
+import { QueueStore } from './QueueStore.js';
+import { StorableRequest } from './StorableRequest.js';
+import './_version.js';
+interface SyncManager {
+    getTags(): Promise<string[]>;
+    register(tag: string): Promise<void>;
+}
+declare global {
+    interface ServiceWorkerRegistration {
+        readonly sync: SyncManager;
+    }
+    interface SyncEvent extends ExtendableEvent {
+        readonly lastChance: boolean;
+        readonly tag: string;
+    }
+    interface ServiceWorkerGlobalScopeEventMap {
+        sync: SyncEvent;
+    }
+}
 /**
- * @module workbox-build
+ * @module workbox-background-sync
  */
-export { copyWorkboxLibraries, generateSW, getManifest, getModuleURL, injectManifest, };
-export * from './types';
+export { BackgroundSyncPlugin, Queue, QueueOptions, QueueStore, StorableRequest };
